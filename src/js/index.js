9150998a2fbe811fcf044839e333b1f44e742eb6
Marcia Agostinho <marciaagostinho27@gmail.com>
2023-01-19 15:09:53 -0300

    changed title

--- src/js/index.js
+++ src/js/index.js
@@ -27,3 +27,16 @@ window.onscroll = () =>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
